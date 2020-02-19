32e6f166354ca0eb11f3eb9a97d85edc754ccd9e
lazaro-programacion <lazaro0089@gmail.com>
2020-02-19 09:20:10 +0100

    first commit

---
+++ src/main/java/com/aplicacionfacturas/aplFacturas/AplFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.aplicacionfacturas.aplFacturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplFacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aplicacionfacturas/aplFacturas/AplFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplicacionfacturas.aplFacturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
